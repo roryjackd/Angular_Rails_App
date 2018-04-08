4aa9142e084607d89d778fb798744435ed2624ed
roryjackd <roryjackd@yahoo.com>
2018-04-07 22:46:19 -0600

    Created mock data, accessed component data

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -9,6 +9,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var DocumentsComponent = /** @class */ (function () {
     function DocumentsComponent() {
+        this.pageTitle = "Document Dashboard";
+        this.documents = [
+            {
+                title: "My First Doc",
+                description: 'asdfsaasdf asdfsdf',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Second Doc",
+                description: 'asdfsaasdf asdfsdf',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Last Doc",
+                description: 'asdfsaasdf asdfsdf',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            }
+        ];
     }
     DocumentsComponent = __decorate([
         core_1.Component({
--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-  
+  pageTitle: string = "Document Dashboard"
+
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: 'asdfsaasdf asdfsdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Second Doc",
+      description: 'asdfsaasdf asdfsdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Last Doc",
+      description: 'asdfsaasdf asdfsdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    }
+  ]
 }
